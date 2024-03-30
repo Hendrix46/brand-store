f5ac23c7dad97711d39a18ef1adb9c8222f15537
Ikramov_21 <oybek.ikramov.official@gmail.com>
2024-03-30 17:44:36 +0500

    added sort functionality

--- src/pages/productList/ProductList.jsx
+++ src/pages/productList/ProductList.jsx
@@ -6,45 +6,38 @@ import {FeaturesCard} from "../../components/cards/FeaturesCard.jsx";
 import {useEffect, useState} from "react";
 
 export const ProductList = () => {
-
     const {
-        data,
+        data = { products: [] },
         error,
         isLoading,
     } = useQuery("products", useGetProductList);
 
-    const [products , setProducts] = useState(null)
-
-    const handleSortByPrice = (val) => {
-        if (val === 'Asc') {
-            setProducts(data.products.sort((a, b) => a.price - b.price))
-        } else if (val === 'Desc') {
-           setProducts(data.products.sort((a, b) => b.price - a.price))
-        }
-    }
-    const handleFilterByName = (val) => {
-        setProducts(data.products.filter(product => product.title.toLocaleLowerCase().includes(val.toLowerCase())))
-    }
-
-    const handleFilterByStockAvailable = (val) => {
-        setProducts(data.products.filter(product => val === 'In Stock' ? product.stock > 0 : product.stock === 0))
-    }
+    const [products , setProducts] = useState([])
+    const [sortByPrice, setSortByPrice] = useState(null);
+    const [filterByStock, setFilterByStock] = useState(null);
+    const [filterByName, setFilterByName] = useState(null);
 
     useEffect(() => {
-        if (!isLoading && data?.products.length){
-            setProducts(data.products)
+        let result = [...data.products];
+        if (filterByName) {
+            result = result.filter(product => product.title.toLocaleLowerCase().includes(filterByName.toLowerCase()));
         }
-    }, []);
-
+        if (filterByStock) {
+            result = result.filter(product => filterByStock === 'In Stock' ? product.stock > 0 : product.stock === 0);
+        }
+        if (sortByPrice) {
+            result.sort((a, b) => sortByPrice === 'Asc' ? a.price - b.price : b.price - a.price);
+        }
+        setProducts(result);
+    }, [data, sortByPrice, filterByStock, filterByName]);
 
     if (isLoading) return (
         <Box>
             <LoadingOverlay visible={isLoading} zIndex={1000} />
         </Box>
     );
-
     if (error) return <div>An error occurred: {error.message}</div>;
-    console.log(products, 'PROD')
+
     return (
         <>
             <Container size={1280} p={32}>
@@ -53,23 +46,23 @@ export const ProductList = () => {
                         data={['iPhone', 'Samsung', 'Huawei', 'Macbook']}
                         label={'Filter by name'}
                         placeholder="Select product name"
-                        onChange={(val)=> handleFilterByName(val)}
+                        onChange={(val)=> setFilterByName(val)}
                     />
                     <SelectField
                         data={['Asc', 'Desc']}
                         label={'Sort by price'}
                         defaultValue={'Asc'}
-                        onChange={(val)=> handleSortByPrice(val)}
+                        onChange={(val)=> setSortByPrice(val)}
                     />
                     <SelectField
                         data={['In Stock', 'Out of Stock']}
                         label={'Sort by stock available'}
                         placeholder="Select option"
-                        onChange={(val)=> handleFilterByStockAvailable(val)}
+                        onChange={(val)=> setFilterByStock(val)}
                     />
                 </Group>
                 <SimpleGrid cols={4}>
-                    {data.products.map(product => (
+                    {products.map(product => (
                         <FeaturesCard
                             key={product.id}
                             {...product}
